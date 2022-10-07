e72ab2a294997231c918701e9996270a7387af4f
XUSANO.D <xusanovd3400@gmail.com>
2022-10-07 07:49:30 +0500

    men html javascript

---
+++ index.diyor/main.diyor/index.001/dior.html/dior.js
@@ -0,0 +1,84 @@
+// let number=10;      /* number data type */
+
+// let a=10;   
+
+// console.log(typeof(a));
+// console.log(number);
+
+
+// let surName='Abbossov';   /* string data type */
+
+// console.log(typeof(surName));
+// console.log(surName);
+
+// let age;     /* undefined data type */
+
+// console.log(age);
+// console.log(typeof(age));
+
+
+// console.log(showMan);     /* null data type */
+// console.log(typeof(showMan));
+
+// const isMerried=false;     /* boolen data type */  
+
+// console.log(isMerried);
+// console.log(typeof(isMerried));
+
+// const arr=[12,"dior", true];
+
+// console.log(arr.length);
+// console.log(typeof(arr));
+
+// const db={
+//     age: 12,
+//     hearColor:"crimsn",
+//     heaght: 1.51,
+//     isMarried: false,
+//     body: "brown",
+//     yoshim: 12,
+//     tugilganYilim: 2010,
+//     surName: "XUSANOV",
+//     name: "DIYORBEK",
+//     TugilganKunim: "11.04.2010",
+//     Karra: "3*3=9",
+//     Boluv: "7/3=21",
+//     Qoshuv: "56+44=100",
+//     Ayru: "99-58=41",
+
+
+// }
+
+
+// console.log(db);
+
+
+    // let locatet=prompt('instagram nomingizni kriting');
+    
+    // console.log("https://Instagram.com/"+locatet);
+
+
+    // let newPaper=prompt("instagram nomingizni kriting");
+
+    // console.log(`https://instagram.com/${newPaper}`);
+
+    // const nameof='Abboss';
+
+    // let hobby="web developer";
+
+    // console.log(nameof+hobby);
+    // console.log(`${nameof} ${hobby}`);
+
+    // let text1=prompt('ismingizni harfini kriting');
+    // let text2=prompt('ismingizni harfini kriting');
+    // let text3=prompt('ismingizni harfini kriting'); 
+    // let text4=prompt('ismingizni harfini kriting');
+    // let text5=prompt('ismingizni harfini kriting');
+    // let text6=prompt('ismingizni harfini kriting');
+    // let text7=prompt('ismingizni harfini kriting');
+    // let text8=prompt('ismingizni harfini kriting');
+    // console.log(`${text1} ${text2} ${text3} ${text4} ${text5} ${text6} ${text7} ${text8}`);
+
+    // interpalatsiya
+
+    
